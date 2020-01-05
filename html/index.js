5c14f5c167acdb45d4adc2ca1114afe414fbd710
vraisamis <quick.sorting.arg@gmail.com>
2020-01-05 23:41:41 +0900

    with lazy_static

--- html/index.js
+++ html/index.js
@@ -2,7 +2,17 @@ const js = import("./node_modules/wasm/wasm.js");
 js.then(js => {
   const e = document.getElementById("btn");
   e.addEventListener('click', () => {
-	const ee = document.getElementById("src");
-	js.analyze(ee.value);
+    const ee = document.getElementById("src");
+    const s = js.analyze(ee.value);
+    alert(s);
   });
 });
+
+// const js = import("./node_modules/wasm/wasm.js");
+// js.then(js => {
+//   const e = document.getElementById("btn");
+//   e.addEventListener('click', () => {
+// 	const ee = document.getElementById("src");
+// 	js.analyze(ee.value);
+//   });
+// });
--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,37 +1,47 @@
+#[macro_use]
+extern crate lazy_static;
 extern crate wasm_bindgen;
 
+use sudachi::morpheme::Morpheme;
+use sudachi::tokenizer::{Mode, Tokenizer};
 use wasm_bindgen::prelude::wasm_bindgen;
-use sudachi::tokenizer::{Tokenizer, Mode};
-
 
 #[wasm_bindgen]
-extern {
+extern "C" {
     pub fn alert(s: &str);
 }
 
 #[wasm_bindgen]
-pub fn analyze(src: &str) {
-    let bytes = include_bytes!("resources/system.dic");
-    let tokenizer = Tokenizer::new(bytes);
-    let mode = Mode::C;
-    let src = src.to_string();
-    let morpheme_list = tokenizer.tokenize(&src, &mode, false);
-    let dst = morpheme_list.into_iter().map(|morpheme| {
-        format!(
-                    "{}\t{}\t{}\t{}\t{}",
-                    morpheme.surface(),
-                    morpheme.pos().join(","),
-                    morpheme.normalized_form(),
-                    morpheme.dictionary_form(),
-                    morpheme.reading_form(),
-               )
-    }).collect::<Vec<String>>().join("\n");
-    alert(&dst);
-}
-
-
-
-#[wasm_bindgen]
 pub fn greet(name: &str) {
     alert(&format!("Hello, {}!", name));
+}
+
+#[wasm_bindgen]
+pub fn analyze(src: &str) -> String {
+    let morpheme_list = morphemes(src);
+    let dst = morpheme_list
+        .into_iter()
+        .map(|morpheme| {
+            format!(
+                "{}\t{}\t{}\t{}\t{}",
+                morpheme.surface(),
+                morpheme.pos().join(","),
+                morpheme.normalized_form(),
+                morpheme.dictionary_form(),
+                morpheme.reading_form(),
+            )
+        })
+        .collect::<Vec<String>>()
+        .join("\n");
+    dst
+}
+
+static BYTES: &'static [u8] = include_bytes!("resources/system.dic");
+lazy_static! {
+    static ref TOKENIZER: Tokenizer<'static> = Tokenizer::new(BYTES);
+}
+
+fn morphemes<'a>(src: &str) -> Vec<Morpheme<'a>> {
+    let src = src.to_string();
+    TOKENIZER.tokenize(&src, &Mode::C, false)
 }
